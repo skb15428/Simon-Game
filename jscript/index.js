38cc85d583500e94187edd0d4cd647dbc1c20571
skb15428 <51283207+skb15428@users.noreply.github.com>
2020-06-13 20:23:05 +0530

    Complete Simon game

---
+++ jscript/index.js
@@ -0,0 +1,81 @@
+var st = "Press 's' to start";
+var pl = "Game started";
+var go = "Game over";
+var comp;
+var player;
+var wrong;
+var level_count;
+var colors = ["red", "green", "yellow", "blue"];
+$(document).keydown(function (event) {
+  if (event.key == "s") {
+    $("h1").html(pl);
+    comp = "";
+    player = "";
+    wrong = false;
+    level_count=1;
+    nextSequence();
+    $("h2").css("visibility","visible");
+  }
+});
+$(".btn").click(function () {
+  if (wrong == false) {
+    var cl = $(this).attr("class");
+    var color_btn = cl.slice(0, cl.indexOf(" "));
+    clicked(color_btn);
+    switch (color_btn) {
+      case "red":
+        player += 0;
+        break;
+      case "green":
+        player += 1;
+        break;
+      case "yellow":
+        player += 2;
+        break;
+      case "blue":
+        player += 3;
+        break;
+    }
+    check_ans();
+  }
+});
+function check_ans() {
+  var len = player.length - 1;
+  if (player[len] === comp[len]) {
+    if (player.length === comp.length) {
+      setTimeout(function () {
+        player="";
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    $("h1").html(go);
+    $("h2").html("Press 's' to restart");
+    var audio = new Audio("sounds/wrong.mp3");
+    audio.play();
+    wrong=true;
+  }
+}
+function nextSequence() {
+    $("h2").html("Level "+level_count);
+    level_count++;
+  comp += Math.floor(Math.random() * 4);
+  console.log(comp);
+  var end = comp.length * 1000;
+  var i = 0;
+  var si = setInterval(function () {
+    clicked(colors[comp[i]]);
+    i++;
+  }, 1000);
+  setTimeout(function () {
+    clearInterval(si);
+  }, end);
+}
+function clicked(str) {
+  var audio = new Audio("sounds/" + str + ".mp3");
+  audio.play();
+  $("." + str).addClass("pressed");
+  setTimeout(function () {
+    $("." + str).removeClass("pressed");
+  }, 400);
+}
